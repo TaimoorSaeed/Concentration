9c6ef4958a1fea8caa321f8c0d74c15836ea054b
TaimoorSaeed <taimoor.dev@gmail.com>
2018-02-27 13:12:01 +0500

    added flip count labels

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -10,29 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // MARK: LABELS
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flip Counts \(flipCount)"
+        }
     }
     
     @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
         
         flipCard(withEmoji: "👻", on: sender)
     }
-    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCount += 1
+        flipCard(withEmoji: "🎃", on: sender)
+    }
     func flipCard(withEmoji emoji: String, on button: UIButton){
         
         if button.currentTitle == emoji {
             button.setTitle("", for: .normal)
             button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         }
-            
-        else{
+        else {
             button.setTitle(emoji, for: .normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-            
         }
-        
         
     }
     
